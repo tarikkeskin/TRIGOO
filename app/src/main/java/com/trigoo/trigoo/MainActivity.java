50722868c97a6553868892d34e1435ba0d471473
tarikkeskin <tarik.keskin007@gmail.com>
2022-05-06 23:56:30 +0300

    Bottom Navigation and Fragments

--- app/src/main/java/com/trigoo/trigoo/MainActivity.java
+++ app/src/main/java/com/trigoo/trigoo/MainActivity.java
@@ -1,14 +1,27 @@
 package com.trigoo.trigoo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.trigoo.trigoo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding tasarim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tasarim = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(tasarim.getRoot());
+
+        //Access to the navigation host
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavigationUI.setupWithNavController(tasarim.bottomNav,navHostFragment.getNavController());
+
     }
 }
